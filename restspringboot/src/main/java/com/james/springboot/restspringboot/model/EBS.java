b6134f4bcfd1a90b70719cd4c1a32c26b0ccc485
James <James@James>
2016-04-10 19:28:35 -0400

    add EBS model

---
+++ restspringboot/src/main/java/com/james/springboot/restspringboot/model/EBS.java
@@ -0,0 +1,43 @@
+package com.james.springboot.restspringboot.model;
+
+public class EBS {
+
+	private int stuId;
+	private String firstName;
+	private String lastName;
+	private int finacialNum;
+	private int teacherId;
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getFinacialNum() {
+		return finacialNum;
+	}
+	public void setFinacialNum(int finacialNum) {
+		this.finacialNum = finacialNum;
+	}
+	public int getTeacherId() {
+		return teacherId;
+	}
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+	
+	
+	
+}
