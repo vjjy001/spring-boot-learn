13eb16f7b9e8842630f882e0e1c3a360dfcb55d2
JIAN_J <JIAN_J@241918LT-DFSMS.psb.bls.gov>
2016-04-11 10:11:00 -0400

    add push json to Rest

--- restspringboot/src/main/java/com/james/springboot/restspringboot/service/action/JdbcTestController.java
+++ restspringboot/src/main/java/com/james/springboot/restspringboot/service/action/JdbcTestController.java
@@ -1,13 +1,17 @@
 package com.james.springboot.restspringboot.service.action;
 
+
 import java.util.Collection;
 import java.util.List;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,5 +70,43 @@ public class JdbcTestController {
 	public ResponseEntity<Collection<State>> getAllStatesByRegionID(@RequestParam(value="blsnum",defaultValue="1") int blsnum){
 			List<State> list =stateDao.getStatesByRegion(blsnum);
 		return new ResponseEntity<Collection<State>>(list,HttpStatus.OK);
+	}
+
+	
+	@RequestMapping(value="/api/pushStates",
+			method=RequestMethod.POST,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<State>> getPagePushData(@RequestBody List<State> list){
+			
+		//List<State> sList  = Arrays.asList(list);
+			//System.out.println(str);
+			/*for(State st:list){
+				System.out.println(st.getStateCode()+st.getStateNum());
+			}*/
+			//list.stream().sorted((s1,s2) ->Integer.parseInt(s1.getStateNum()) >Integer.parseInt(s2.getStateNum()));
+		//return str;
+			return new ResponseEntity<Collection<State>>(list,HttpStatus.OK);
+	}
+
+	
+	@RequestMapping(value="/api/pushStatesStr",
+			method=RequestMethod.POST
+			)
+	public ResponseEntity<String> getPagePushDataStr(@RequestBody() String strs){
+			System.out.println(strs);
+			
+			
+		return new ResponseEntity<String>(strs,HttpStatus.OK);
+			
+	}
+	
+	@RequestMapping(value="/api/pushStatesGet/",
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<State>> getPushDataGet(@PathVariable List<State> list){
+			
+	
+		//return str;
+			return new ResponseEntity<Collection<State>>(list,HttpStatus.OK);
 	}
 }
