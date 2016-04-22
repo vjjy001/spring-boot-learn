1b34e24cd0d35a9bfa30817af8fbbf1446207c5d
JIAN_J <JIAN_J@241918LT-DFSMS.psb.bls.gov>
2016-04-22 16:20:21 -0400

    accept the cross support for xmlhttprequest

--- restspringboot/src/main/java/com/james/springboot/restspringboot/service/action/JdbcTestController.java
+++ restspringboot/src/main/java/com/james/springboot/restspringboot/service/action/JdbcTestController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,13 @@ import com.james.springboot.restspringboot.dao.IStateDao;
 import com.james.springboot.restspringboot.model.State;
 
 
-
+@CrossOrigin
 @RestController
 public class JdbcTestController {
 	
 	@Autowired
 	private IStateDao stateDao;
+	
 	
 	@RequestMapping(value="/api/states",
 			method=RequestMethod.GET,
