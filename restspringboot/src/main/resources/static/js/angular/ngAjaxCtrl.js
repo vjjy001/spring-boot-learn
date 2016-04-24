d10bf1b772d4edce02ba317ba77e278ac40ff388
James <James@James>
2016-04-24 10:02:52 -0400

    try to push to master

---
+++ restspringboot/src/main/resources/static/js/angular/ngAjaxCtrl.js
@@ -0,0 +1,24 @@
+var app = angular.module("ngApp",[]);
+
+app.controller("ngCtrl",function($scope,$http){
+	
+	
+	$scope.getStates = function(){
+		
+		$http.get('http://localhost:8080/api/states').then(function success(response){
+			
+			console.log(response.status);
+			if(response.status == 200)
+			{
+				$scope.ok = true;
+				}
+			$scope.recvData = response.data;
+			
+		},function fail(response){
+			
+			console.log(response.status);
+			
+		});
+	}
+	
+});
