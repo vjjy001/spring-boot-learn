8f11c1851dbef44e9b05ebdb8abd68c011f16422
James <James@James>
2016-04-24 11:06:38 -0400

    add the local angular ajax call

--- restspringboot/src/main/resources/static/js/angular/ngAjaxCtrl.js
+++ restspringboot/src/main/resources/static/js/angular/ngAjaxCtrl.js
@@ -22,3 +22,25 @@ app.controller("ngCtrl",function($scope,$http){
 	}
 	
 });
+
+app.controller("ngLocalCtrl", function($scope,$http){
+	
+	$scope.getEmp=function(){
+		$http.get('http://localhost:8080/api/getStudents').then(function success(response){
+			
+			console.log(response.status);
+			if(response.status == 200)
+			{
+				$scope.ok = true;
+				}
+			$scope.recvData = response.data;
+			
+		},function fail(response){
+			
+			console.log(response.status);
+			
+		});
+	}
+	
+	
+});
